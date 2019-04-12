1982298912519486c3ca989bf6669c0418fac2be
Johnson Joseph <jhonsonj88@gmail.com>
2019-04-12 13:59:28 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,4 @@
 
 ActiveRecord::Schema.define(version: 2019_04_07_165304) do
 
-  create_table "movies", force: :cascade do |t|
-    t.string "title"
-    t.integer "release_date"
-    t.string "director"
-    t.string "lead"
-    t.boolean "in_theaters"
-  end
-
 end
